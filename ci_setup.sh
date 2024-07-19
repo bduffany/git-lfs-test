8dfa6e03483ab296a80a352c24ed36faf104691f
Brandon Duffany <brandon@buildbuddy.io>
2024-07-19 13:42:47 -0400

    Don't install git lfs if already installed

--- ci_setup.sh
+++ ci_setup.sh
@@ -3,8 +3,11 @@ set -euo pipefail
 
 cd "$BUILD_WORKSPACE_DIRECTORY"
 
-export DEBIAN_FRONTEND=noninteractive
-sudo apt update
-sudo apt install -y git-lfs
+if ! command -v git-lfs &>/dev/null; then
+  export DEBIAN_FRONTEND=noninteractive
+  sudo apt update
+  sudo apt install -y git-lfs
+fi
+
 git lfs pull
 
