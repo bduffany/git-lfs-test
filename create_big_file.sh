2a702c59e450ef36755bf0e3f09888a4129e0de1
Brandon Duffany <brandon@buildbuddy.io>
2024-07-19 14:50:55 -0400

    Make max file size configurable

--- create_big_file.sh
+++ create_big_file.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
-FILE_SIZE=$(python3 -c 'import random; print(random.randint(10_000, 4_000_000), end="")')
+: "${BIG_FILE_MAX_SIZE:=4000000}"
+FILE_SIZE=$(python3 -c "import random; print(random.randint(10_000, $BIG_FILE_MAX_SIZE), end='')")
 mkdir -p big
 head -c "$FILE_SIZE" </dev/random >big/$(uuidgen).big
 
